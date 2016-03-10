469ed5a8b220c2356b703e765066cbf791143729
Divij Bindlish <dvjbndlsh93@gmail.com>
2016-03-11 03:27:20 +0530

    Add style to waveform

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function () {
     var selectedSample = undefined;
     var selectedAlgorithm = undefined;
+    var waves = {};
 
     $('.content-top .pure-button').on('click', function () {
         $this = $(this);
@@ -11,9 +12,50 @@ $(document).on('ready', function () {
         $('.content-top .pure-button-active').removeClass('pure-button-active');
         $this.addClass('pure-button-active');
 
+        $('.content-algorithm').show();
+
+        $('.content-waveforms').show();
+
         selectedSample = sample;
         console.log('Selected: ' + sample);
-        console.log('TODO: Fetch and display signals + play');
+
+        firstPass = false;
+        var signals = ['os1', 'os2', 'ms1', 'ms2'];
+        var urls = ['/original/1', '/original/2', '/mixture/1', '/mixture/2'];
+
+        for (var i = 0; i < 4; i++) {
+            var signal = signals[i];
+            var url = '/oct/sound/' + sample + urls[i];
+
+            var wave = WaveSurfer.create({
+                container: '#' + signal,
+                waveColor: 'violet',
+                progressColor: 'purple'
+            });
+
+            wave.load(url);
+            waves[signal] = wave;
+        }
+
+        $('.content-waveforms .play-pause').on('click', function () {
+            var $this = $(this);
+            var signalToToggle = $this.attr('signal');
+            var wave = waves[signalToToggle];
+            wave.playPause();
+            if ($this.html() == 'Play') {
+                $this.html('Pause');
+            } else {
+                $this.html('Play');
+            }
+        });
+
+        $('.content-waveforms .slider').on('input', function () {
+            var $this = $(this);
+            var signalToToggle = $this.attr('signal');
+            var wave = waves[signalToToggle];
+            var zoomLevel = Number($this.val());
+            wave.zoom(zoomLevel);
+        })
     });
 
     $('.content-algorithm .pure-button').on('click', function () {
