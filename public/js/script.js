6c89d88db552e98113c4396d40e651110f863a44
Divij Bindlish <dvjbndlsh93@gmail.com>
2016-03-11 02:40:56 +0530

    Add buttons to select algorithm

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
 $(document).on('ready', function () {
+    var selectedSample = undefined;
+    var selectedAlgorithm = undefined;
+
     $('.content-top .pure-button').on('click', function () {
         $this = $(this);
 
@@ -8,8 +11,28 @@ $(document).on('ready', function () {
         $('.content-top .pure-button-active').removeClass('pure-button-active');
         $this.addClass('pure-button-active');
 
+        selectedSample = sample;
         console.log('Selected: ' + sample);
         console.log('TODO: Fetch and display signals + play');
+    });
+
+    $('.content-algorithm .pure-button').on('click', function () {
+        if (selectedSample === undefined) {
+            console.log('Wrong click');
+            return;
+        }
+
+        $this = $(this);
+
+        if ($this.hasClass('pure-button-active')) return;
+
+        var algorithm = $this.attr('algorithm');
+        $('.content-algorithm .pure-button-active').removeClass('pure-button-active');
+        $this.addClass('pure-button-active');
+
+        selectedAlgorithm = algorithm;
+        console.log('Algorithm: ' + algorithm);
+        console.log('TODO: Fetch results');
     });
 
 });
